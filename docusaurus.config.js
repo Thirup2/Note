c8537fd69b4a8f942dbeb7dbc985fcf98cdf9431
Thirup2 <246946332@qq.com>
2023-11-04 18:30:06 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const math = require('remark-math');
 const katex = require('rehype-katex');
 
@@ -232,8 +230,8 @@ const config = {
         copyright: `Copyright © 2022 - ${new Date().getFullYear()} | <a href="https://note.syunn.cn">NOTE.SYUNN.CN</a> | Built with Docusaurus<br /><a href="https://icp.gov.moe/?keyword=20233201" target="_blank" style="display: inline-block; vertical-align: middle;"><img src="/img/moeicp.ico" alt="moeicp.ico" style="display: inline-block; height: 1.3em; width: auto; vertical-align: middle;" />萌ICP备20233201号</a>`,
       },
       prism: {
-        theme: require('prism-react-renderer/themes/github'),
-        darkTheme: darkCodeTheme,
+        theme: require('prism-react-renderer/themes/nightOwlLight'),
+        darkTheme: require('prism-react-renderer/themes/nightOwl'),
         magicComments: [
           {
             className: 'theme-code-block-highlighted-line',
